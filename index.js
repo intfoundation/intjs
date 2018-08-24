a83a367f472e1b92e0cf8478e1514482a6362459
like <likeaixi@gmail.com>
2018-08-24 18:08:47 +0800

    rename

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 /*!
 *  intjs - INT JavaScript API
-*  https://github.com/intfoundation/inttools.js.git
+*  https://github.com/intfoundation/intjs.git
 *
 * Permission is hereby granted, free of charge, to any person obtaining a copy
 * of this software and associated documentation files (the "Software"), to deal
